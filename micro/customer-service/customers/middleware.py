c9ea80d3f1e9f5040d14133ef7465d012bcb87dc
Khanh091 <nvkhanh0911@gmail.com>
2026-01-18 21:03:10 +0700

    add middleware

---
+++ micro/customer-service/customers/middleware.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+
+class CorsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Handle preflight OPTIONS request
+        if request.method == 'OPTIONS':
+            response = JsonResponse({})
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+            response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+            return response
+        
+        response = self.get_response(request)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
