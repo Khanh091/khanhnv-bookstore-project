b04d2f7843dcedb0f41e9a28fda8d211cf4a3b4f
Khanh091 <nvkhanh0911@gmail.com>
2026-01-18 20:52:12 +0700

    them fe rieng

---
+++ micro/frontend/config.js
@@ -0,0 +1,12 @@
+// API endpoints configuration
+const API_CONFIG = {
+    CUSTOMER_SERVICE: 'http://127.0.0.1:8001',
+    BOOK_SERVICE: 'http://127.0.0.1:8002',
+    CART_SERVICE: 'http://127.0.0.1:8003'
+};
+
+// Local storage keys
+const STORAGE_KEYS = {
+    CUSTOMER_ID: 'customer_id',
+    CUSTOMER_NAME: 'customer_name'
+};
